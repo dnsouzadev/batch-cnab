658213bcaf226ffad176ade3e7e755bd88827116
Daniel Souza <workdndsza@gmail.com>
2024-07-02 16:17:21 -0300

    iniciando o backend com spring batch

---
+++ backend/src/main/java/com/dnsouzadev/backend/BatchConfig.java
@@ -0,0 +1,15 @@
+package com.dnsouzadev.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class BatchConfig {
+    private PlatformTransactionManager transactionManager;
+
+    public BatchConfig(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    
+}
