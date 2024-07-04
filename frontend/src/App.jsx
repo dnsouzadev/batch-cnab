d5d863765c2c6209d9e70e3d2d879ecb64e26204
Daniel Souza <workdndsza@gmail.com>
2024-07-04 12:03:21 -0300

    estilizando com o tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,92 +1,170 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import { faSync } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [ transactions, setTransactions ] = useState([])
-  const [ file, setFile ] = useState(null)
 
-  const fetchTransactions = async () => {
-    const response = await axios.get("http://localhost:8080/transacoes")
-    setTransactions(response.data)
-    console.log(response.data)
-  }
+const App = () => {
+  const fetchURL = "http://localhost:8080/transacoes";
+  const uploadURL = "http://localhost:8080/cnab/upload";
+
+  const [file, setFile] = useState(null);
+  const [reports, setReports] = useState([]);
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleFileChange = (e) => {
-    setFile(e.target.files[0])
-  }
+    setFile(e.target.files[0]);
+    setSuccess(e.target.files[0].name);
+  };
 
   const uploadFile = async () => {
-    const formData = new FormData()
-    formData.append("file", file)
-    axios.post("http://localhost:8080/cnab/upload", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data"
-      }
-    }).then(() => {
-      fetchTransactions()
-    })
-  }
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const response = await axios.post(uploadURL, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      setError(null);
+      setSuccess(response.data);
+
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      setError(error.response.data);
+    }
+  };
+
+  const fetchTransactions = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(fetchURL);
+      setReports(response.data);
+    } catch (error) {
+      console.error('Error fetching transactions:', error);
+    } finally {
+      setIsLoading(false);
+      setError(null);
+      setSuccess(null);
+    }
+  };
 
   useEffect(() => {
-    fetchTransactions()
-  }, [])
+    fetchTransactions();
+  }, []);
+
+  const formatCurrency = (value) => {
+    const formattedValue = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(parseFloat(value));
+
+    return formattedValue;
+  };
 
   return (
-    <div>
-      <div>
-        <h1>Importacao de CNAB</h1>
-      </div>
-      <div>
-        <span>Choose FIle</span>
-        <input type="file"
-        accept=".txt"
-        onChange={handleFileChange}
-        />
-        <button
-        onClick={uploadFile}
-        >Upload File</button>
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold mb-4">Importação de CNAB</h1>
+
+      {/* File Upload Form */}
+      <div className="mb-8">
+        <div className="flex items-center space-x-4">
+          <label className="text-gray-600">
+            <span className="bg-blue-500 hover:bg-blue-600 cursor-pointer text-white py-2 px-4 rounded-lg">
+              Choose File
+            </span>
+            <input
+              type="file"
+              className="hidden"
+              onChange={handleFileChange}
+              accept=".txt"
+            />
+          </label>
+          <button
+            className="bg-blue-700 hover:bg-blue-800 text-white py-2 px-4 rounded-lg"
+            onClick={uploadFile}
+          >
+            Upload File
+          </button>
+        </div>
       </div>
 
-      <div>
-        <h2>Transactions</h2>
-        <ul>
-          { transactions.map((report, key) => (
-            <li key={key}>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Cartao</th>
-                    <th>CPF</th>
-                    <th>Data</th>
-                    <th>Dono da Loja</th>
-                    <th>Hora</th>
-                    <th>Nome da Loja</th>
-                    <th>Tipo</th>
-                    <th>Valor</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  { report.transacoes.map((transaction, key) => (
-                    <tr key={key}>
-                      <td>{transaction.cartao}</td>
-                      <td>{transaction.cpf}</td>
-                      <td>{transaction.data}</td>
-                      <td>{transaction.donoDaLoja}</td>
-                      <td>{transaction.hora}</td>
-                      <td>{transaction.nomeDaLoja}</td>
-                      <td>{transaction.tipo}</td>
-                      <td>{transaction.valor}</td>
+      {/* Form Messages */}
+      {success && <p className="text-green-500 mt-2">{success}</p>}
+      {error && <p className="text-red-500 mt-2">{error}</p>}
+
+      <br />
+
+      {/* Refresh Button */}
+      <button
+        className="bg-gray-200 hover:bg-gray-300 text-gray-600 py-2 px-4 rounded-lg mb-4"
+        onClick={fetchTransactions}
+        disabled={isLoading} // Disable while loading
+      >
+        <FontAwesomeIcon icon={faSync} spin={isLoading} className="mr-2" />
+        {isLoading ? 'Atualizando...' : 'Atualizar Transações'}
+      </button>
+
+      {/* Transaction List */}
+      <div className="p-4">
+        <h2 className="text-2xl font-semibold mb-4">Transações</h2>
+        <ul className="bg-white shadow-md rounded-md p-4">
+          {reports.length === 0 ? (
+            <p className="mb-4 text-gray-500 text-center">Sem transações disponíveis.</p>
+          ) : (
+
+            reports.map((report, key) => (
+              <li
+                key={key}
+                className="mb-4 p-4 border-b border-gray-300 flex flex-col"
+              >
+                <div className="flex justify-between items-center mb-2">
+                  <div className="text-xl font-semibold">{report.nomeDaLoja}</div>
+                  <div className={`text-${parseFloat(report.total) >= 0 ? 'green' : 'red'}-500 font-semibold`}>
+                    Total: {formatCurrency(parseFloat(report.total))}
+                  </div>
+                </div>
+
+                {/* Transaction Details Table */}
+                <table className="table-auto w-full">
+                  <thead>
+                    <tr>
+                      <th className="px-4 py-2">Cartão</th>
+                      <th className="px-4 py-2">CPF</th>
+                      <th className="px-4 py-2">Data</th>
+                      <th className="px-4 py-2">Dono da Loja</th>
+                      <th className="px-4 py-2">Hora</th>
+                      <th className="px-4 py-2">Nome da Loja</th>
+                      <th className="px-4 py-2">Tipo</th>
+                      <th className="px-4 py-2">Valor</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </li>
-          ))}
+                  </thead>
+                  <tbody>
+                    {report.transacoes.map((transacao, index) => (
+                      <tr key={index} className={index % 2 === 0 ? 'bg-gray-100' : ''}>
+                        <td className="px-4 py-2">{transacao.cartao}</td>
+                        <td className="px-4 py-2">{transacao.cpf}</td>
+                        <td className="px-4 py-2">{transacao.data}</td>
+                        <td className="px-4 py-2">{transacao.donoDaLoja}</td>
+                        <td className="px-4 py-2">{transacao.hora}</td>
+                        <td className="px-4 py-2">{transacao.nomeDaLoja}</td>
+                        <td className="px-4 py-2">{transacao.tipo}</td>
+                        <td className="px-4 py-2">{formatCurrency(transacao.valor)}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </li>
+            ))
+          )}
         </ul>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
