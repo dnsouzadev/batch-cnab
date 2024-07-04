ef3e25f9bf32b717c0e8c6693b74b26b612d3e7d
Daniel Souza <workdndsza@gmail.com>
2024-07-04 11:55:09 -0300

    integrando com o backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,35 @@
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
   const [ transactions, setTransactions ] = useState([])
+  const [ file, setFile ] = useState(null)
 
   const fetchTransactions = async () => {
     const response = await axios.get("http://localhost:8080/transacoes")
     setTransactions(response.data)
+    console.log(response.data)
   }
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0])
+  }
+
+  const uploadFile = async () => {
+    const formData = new FormData()
+    formData.append("file", file)
+    axios.post("http://localhost:8080/cnab/upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    }).then(() => {
+      fetchTransactions()
+    })
+  }
+
+  useEffect(() => {
+    fetchTransactions()
+  }, [])
 
   return (
     <div>
@@ -17,41 +39,49 @@ function App() {
       <div>
         <span>Choose FIle</span>
         <input type="file"
-        accept=".txt" />
-        <button>Upload File</button>
+        accept=".txt"
+        onChange={handleFileChange}
+        />
+        <button
+        onClick={uploadFile}
+        >Upload File</button>
       </div>
 
       <div>
         <h2>Transactions</h2>
         <ul>
-          <li>
-            <table>
-              <thead>
-                <tr>
-                  <th>Cartao</th>
-                  <th>CPF</th>
-                  <th>Data</th>
-                  <th>Dono da Loja</th>
-                  <th>Hora</th>
-                  <th>Nome da Loja</th>
-                  <th>Tipo</th>
-                  <th>Valor</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1234</td>
-                  <td>12345678901</td>
-                  <td>2021-08-01</td>
-                  <td>Dono da Loja</td>
-                  <td>10:00:00</td>
-                  <td>Nome da Loja</td>
-                  <td>Debito</td>
-                  <td>100.00</td>
-                </tr>
-              </tbody>
-            </table>
-          </li>
+          { transactions.map((report, key) => (
+            <li key={key}>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Cartao</th>
+                    <th>CPF</th>
+                    <th>Data</th>
+                    <th>Dono da Loja</th>
+                    <th>Hora</th>
+                    <th>Nome da Loja</th>
+                    <th>Tipo</th>
+                    <th>Valor</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { report.transacoes.map((transaction, key) => (
+                    <tr key={key}>
+                      <td>{transaction.cartao}</td>
+                      <td>{transaction.cpf}</td>
+                      <td>{transaction.data}</td>
+                      <td>{transaction.donoDaLoja}</td>
+                      <td>{transaction.hora}</td>
+                      <td>{transaction.nomeDaLoja}</td>
+                      <td>{transaction.tipo}</td>
+                      <td>{transaction.valor}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </li>
+          ))}
         </ul>
       </div>
 
