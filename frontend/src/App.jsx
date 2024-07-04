bdd15eb0f9c39d36ecd3391b7494384aa6f35f6a
Daniel Souza <workdndsza@gmail.com>
2024-07-04 11:34:57 -0300

    estruturando

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios"
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ transactions, setTransactions ] = useState([])
+
+  const fetchTransactions = async () => {
+    const response = await axios.get("http://localhost:8080/transacoes")
+    setTransactions(response.data)
+  }
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Importacao de CNAB</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <span>Choose FIle</span>
+        <input type="file"
+        accept=".txt" />
+        <button>Upload File</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div>
+        <h2>Transactions</h2>
+        <ul>
+          <li>
+            <table>
+              <thead>
+                <tr>
+                  <th>Cartao</th>
+                  <th>CPF</th>
+                  <th>Data</th>
+                  <th>Dono da Loja</th>
+                  <th>Hora</th>
+                  <th>Nome da Loja</th>
+                  <th>Tipo</th>
+                  <th>Valor</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1234</td>
+                  <td>12345678901</td>
+                  <td>2021-08-01</td>
+                  <td>Dono da Loja</td>
+                  <td>10:00:00</td>
+                  <td>Nome da Loja</td>
+                  <td>Debito</td>
+                  <td>100.00</td>
+                </tr>
+              </tbody>
+            </table>
+          </li>
+        </ul>
+      </div>
+
+    </div>
   )
 }
 
